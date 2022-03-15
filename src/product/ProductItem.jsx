2d3847805652983aebf04a79c1bd5f96e7c2e4dd
Chuying He <heidihcy@outlook.com>
2022-03-15 22:35:03 +0100

    add picture in ProductList View

--- src/product/ProductItem.jsx
+++ src/product/ProductItem.jsx
@@ -1,14 +1,21 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { Card, Button } from 'react-bootstrap';
 
 class ProductItem extends Component {
-    state = {  } 
+    state = { firstPictureUrl: "" } 
+
+    componentDidMount() {
+        console.log("ProductItem = ", this.props.product)
+        this.setState({ firstPictureUrl: this.props.product['attributes']['media']['data'][0]['attributes']['url']})
+    }
+
     render() { 
         return (<>
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={this.state.firstPictureUrl} />
             <Card.Body>
-                <Card.Title>Card Title</Card.Title>
+                <Card.Title>{this.props.product['attributes']['name']}</Card.Title>
                 <Card.Text>
                 Some quick example text to build on the card title and make up the bulk of
                 the card's content.
--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import ProductItem from './ProductItem';
+import { Col, Container, Row } from 'react-bootstrap';
 
 class ProductList extends Component {
     state = { 
@@ -12,16 +14,29 @@ class ProductList extends Component {
     }
 
     async getProducts() {
-        const res = await axios.get('/api/products');
+        const res = await axios.get('/api/products?populate=media');
         const products = await res;
-        this.setState({products: products})
-        console.log("PRODUCT", this.state.products)
+        this.setState({products: products['data']['data']})
+        console.log("ProductList = ", this.state.products)
     }
 
-    render() { 
+    render() {
+        //TODO: how to loop through all so that they fit in gird view?
         return (<>
-        PRODUCT
+            <Container>
+                <Row>
+                    <Col>
+                        {
+                            this.state.products.map((item, index) => {
+                                return <ProductItem product={item} />
+                            })
+                        }
+                    </Col>
+                </Row>
+            </Container>
         </>);
+
+        
     }
 }
  
