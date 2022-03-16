e857e59bff68aa72f8fa1681ce3092f1f05adbb9
Chuying He <heidihcy@outlook.com>
2022-03-16 13:59:35 +0100

    add carousel in detail page

--- src/product/ProductDetail.jsx
+++ src/product/ProductDetail.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, Image, Badge, Stack, Card } from 'react-bootstrap';
+import { Container, Row, Col, Image, Badge, Stack, Carousel, Button } from 'react-bootstrap';
+import './_ProductDetail.scss'
 
 class ProductDetail extends Component {
     state = {}
@@ -9,33 +10,77 @@ class ProductDetail extends Component {
         <Container>
             <Row>
                 <Col>
-                    <Image style={{ width: "100%" }} src='https://picsum.photos/200/300'>
-                    </Image>
+                    <Carousel>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://picsum.photos/300/300"
+                            alt="First slide"
+                            />
+                        </Carousel.Item>
+                        <Carousel.Item>
+                            <img
+                            className="d-block w-100"
+                            src="https://picsum.photos/300/300"
+                            alt="Second slide"
+                            />
+                        </Carousel.Item>
+                    </Carousel>
+
                 </Col>
                 <Col>
-                    <Badge bg="dark" className='ap-margin-bottom'>Seife</Badge>
-                    <h1>Kaffee seife</h1>
+                    {/* TODO: width vw */}
+                    <div>
+                        <Badge bg="dark" className='detail-mb'>Seife</Badge>
+                        <h1>Kaffee seife</h1>
 
-                    <div className='ap-margin-bottom'>This is the description of the soap. 
-                        This is the description of the soap. 
-                        This is the description of the soap. 
-                        This is the description of the soap. This is the description of the soap. 
-                        This is the description of the soap. 
-                        This is the description of the soap. 
-                        This is the description of the soap. </div>
+                        <div className='detail-mb'>This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. </div>
 
-                    {/* EFFECT */}
-                    <Stack direction="horizontal" gap={3}>
-                        <Badge bg="success">Pure skin</Badge>
-                        <Badge bg="success">Deep Cleaning</Badge>
-                        <Badge bg="success">Peeling</Badge>
-                    </Stack>
+                        {/* EFFECT */}
+                        <Stack className='detail-mb' direction="horizontal" gap={3}>
+                            <Badge bg="success">Pure skin</Badge>
+                            <Badge bg="success">Deep Cleaning</Badge>
+                            <Badge bg="success">Peeling</Badge>
+                        </Stack>
+
+                        {/* TODO: tsparticles: https://www.npmjs.com/package/react-tsparticles */}
+                        <div className="d-grid gap-2">
+                            <Button variant="secondary" size="lg">
+                                Legen in Einkaufwagen
+                            </Button>
+                        </div>
+                    </div>
+
                 </Col>
             </Row>
+
             <Row>
-                <Col>1 of 3</Col>
-                <Col>2 of 3</Col>
-                <Col>3 of 3</Col>
+                <Col>
+                    <div className='detail-mb'>This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap.This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap.This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. 
+                            This is the description of the soap. </div>
+
+                </Col>
             </Row>
         </Container>
         </>);
